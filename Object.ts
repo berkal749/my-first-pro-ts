b1145ccc654ef183d0b331256b503a8835b4e1f3
berkal749 <berkalachrefpro@gmail.com>
2025-08-31 14:41:03 +0100

    Add initial Object.ts file with user object and function definitions

---
+++ Object.ts
@@ -0,0 +1,13 @@
+let user = {
+  name: "qq",
+  number: 88,
+};
+function ob({ name: string, n: boolean }): object {
+  return {};
+}
+ob({name:"hhh",n:true});
+function reObject():{name:string}{
+return{name:"lol"};
+
+}
+export {};
