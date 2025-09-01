f2f3917ff19b2265e7e3ae53f4f72b848ac5162c
berkal749 <berkalachrefpro@gmail.com>
2025-09-01 15:46:42 +0100

    ok

---
+++ src/classes.ts
@@ -0,0 +1,23 @@
+class user {
+  // protected  email:string;
+  //private readonly name:number;
+
+  constructor(public readonly email: string, public name: number) {}
+  get getEmail(): string {
+    return `lol is ${this.email}`;
+  }
+  set setName(name: number) {
+    if (name < 1) {
+      throw new Error("lol");
+    }
+    this.name = name;
+  }
+}
+class me extends user{
+isFamli:boolean;
+
+
+}
+
+const me = new user("ok", 77);
+me.name;
