5146b8bc28de8f4d2a2e549b13bbd846c805f989
berkal749 <berkalachrefpro@gmail.com>
2025-09-01 13:30:05 +0100

    Add Charcter enum with speech and attack values in enum.ts

---
+++ enum.ts
@@ -0,0 +1,10 @@
+const enum Charcter{ // just use const
+
+//u must put values to all of them not just one
+    Speech="enums are used to make the code even easier to read",
+    attack="ENUMs are soo pro "
+}
+
+
+
+const talkOne =Charcter.Speech;
